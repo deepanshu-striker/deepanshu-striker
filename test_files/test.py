7a15b23e7a7a6d8ac879937e54508c5e9a286ddb
GitHub Action <action@github.com>
2023-05-28 08:33:51 +0000

    Add docstrings to .py files

--- test_files/test.py
+++ test_files/test.py
@@ -1,2 +1,13 @@
-def add_numbers(x,y):
-    return x+y
+def add_numbers(x, y):
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        x (int): First number to be added.
+        y (int): Second number to be added.
+
+    Returns:
+        int: The sum of x and y.
+
+    """
+    return x + y
